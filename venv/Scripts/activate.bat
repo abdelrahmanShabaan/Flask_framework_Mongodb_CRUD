3223af90a3e8dfdf3372c4033a96b8623bdaeb42
abdelrahmanShaaban <boodyelmasry6@gmail.com>
2024-02-26 20:41:41 +0200

    finish labs of flask with mongodb

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\ITI Minya\flask\labs\lab2\venv
+set VIRTUAL_ENV=F:\ITI Minya\flask\labs\labs_flask\lab2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
